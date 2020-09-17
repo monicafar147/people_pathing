877d23af18853cf6658cb46466860c1c0172bc6b
monicafar147 <60337759+monicafar147@users.noreply.github.com>
2020-09-17 18:29:20 +0200

    Update demo_rekognition.py
    focus only on people_pathing

--- demo_rekognition.py
+++ demo_rekognition.py
@@ -74,57 +74,6 @@ class VideoDetect:
 
         return succeeded
 
-    def StartLabelDetection(self):
-        response=self.rek.start_label_detection(Video={'S3Object': {'Bucket': self.bucket, 'Name': self.video}},
-            NotificationChannel={'RoleArn': self.roleArn, 'SNSTopicArn': self.snsTopicArn})
-
-        self.startJobId=response['JobId']
-        print('Start Job Id: ' + self.startJobId)
-
-
-    def GetLabelDetectionResults(self):
-        maxResults = 10
-        paginationToken = ''
-        finished = False
-
-        while finished == False:
-            response = self.rek.get_label_detection(JobId=self.startJobId,
-                                            MaxResults=maxResults,
-                                            NextToken=paginationToken,
-                                            SortBy='TIMESTAMP')
-
-            print('Codec: ' + response['VideoMetadata']['Codec'])
-            print('Duration: ' + str(response['VideoMetadata']['DurationMillis']))
-            print('Format: ' + response['VideoMetadata']['Format'])
-            print('Frame rate: ' + str(response['VideoMetadata']['FrameRate']))
-            print()
-
-            for labelDetection in response['Labels']:
-                label=labelDetection['Label']
-
-                print("Timestamp: " + str(labelDetection['Timestamp']))
-                print("   Label: " + label['Name'])
-                print("   Confidence: " +  str(label['Confidence']))
-                print("   Instances:")
-                for instance in label['Instances']:
-                    print ("      Confidence: " + str(instance['Confidence']))
-                    print ("      Bounding box")
-                    print ("        Top: " + str(instance['BoundingBox']['Top']))
-                    print ("        Left: " + str(instance['BoundingBox']['Left']))
-                    print ("        Width: " +  str(instance['BoundingBox']['Width']))
-                    print ("        Height: " +  str(instance['BoundingBox']['Height']))
-                    print()
-                print()
-                print ("   Parents:")
-                for parent in label['Parents']:
-                    print ("      " + parent['Name'])
-                print ()
-
-                if 'NextToken' in response:
-                    paginationToken = response['NextToken']
-                else:
-                    finished = True
-
     # ============== People pathing ===============  
     def StartPersonPathing(self):
         response=self.rek.start_person_tracking(Video={'S3Object': {'Bucket': self.bucket, 'Name': self.video}},
