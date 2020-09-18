dea71695b16d6ab321355c985914087b1c6e0037
monicafar147 <60337759+monicafar147@users.noreply.github.com>
2020-09-18 12:16:32 +0200

    uploaded video to streamlit

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ def main():
     st.title('''Epsilon tech presents: Q analytics''')
     st.sidebar.radio('select an option',['Track all people', 'Select people to track'])
 
+    # upload video
+    video_file = open('resources/queue_two_people.mp4', 'rb')
+    video_bytes = video_file.read()
+
+    st.video(video_bytes)
+
 if __name__ == "__main__":
     main()
