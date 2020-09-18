5637e374f696297689845038229fdf5551dae2ce
monicafar147 <60337759+monicafar147@users.noreply.github.com>
2020-09-18 12:08:17 +0200

    added sidebar to select people to track

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.write('our app')
+def main():
+    st.title('''Epsilon tech presents: Q analytics''')
+    st.sidebar.radio('select an option',['Track all people', 'Select people to track'])
+
+if __name__ == "__main__":
+    main()
